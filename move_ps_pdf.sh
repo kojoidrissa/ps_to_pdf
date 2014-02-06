2fd85106fb0d6324a1ac24cc1c615085248b4638
Kojo Idrissa <kidrissa@mcdermott.com>
2014-02-06 12:21:11 -0600

    fixed move script; path had a space in the name

--- move_ps_pdf.sh
+++ move_ps_pdf.sh
@@ -1,2 +1,2 @@
-mv *.ps m/Dbsteam/BUDGET/Jackie/qtr_rpt/Q4 2013
-mv *.pdf m/Dbsteam/BUDGET/Jackie/qtr_rpt/Q4 2013
+mv *.ps /m/Dbsteam/BUDGET/Jackie/qtr_rpt/Q4\ 2013/
+mv *.pdf /m/Dbsteam/BUDGET/Jackie/qtr_rpt/Q4\ 2013/
