5096d855f884ae84543c5164b7ef5ddce4a467e9
Kojo Idrissa <kidrissa@mcdermott.com>
2014-04-08 08:37:13 -0500

    Updated the ps_pdf_gen.sh file. I need to change the comments

--- ps_pdf_gen.sh
+++ ps_pdf_gen.sh
@@ -1,5 +1,9 @@
-cp /m/Dbsteam/BUDGET/Jackie/qtr_rpt/*.ps .
+#cp /m/Dbsteam/BUDGET/Jackie/qtr_rpt/*.ps .
+#Run this from the Canopy shell, as it deals with networked drives
+#It also handles the long path names better than GitBash
+
 
 for i in $(ls *.ps);
 do ps2pdfwr $i;
 done
+clear
