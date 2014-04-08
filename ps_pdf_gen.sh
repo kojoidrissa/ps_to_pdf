afb09abf5dd5ef7d174e029f5d88439c03a5ee09
Kojo Idrissa <kidrissa@mcdermott.com>
2014-04-08 08:46:09 -0500

    Updated the comments in ps_pdf_gen.sh and slight formatting change

--- ps_pdf_gen.sh
+++ ps_pdf_gen.sh
@@ -1,9 +1,11 @@
 #cp /m/Dbsteam/BUDGET/Jackie/qtr_rpt/*.ps .
 #Run this from the Canopy shell, as it deals with networked drives
 #It also handles the long path names better than GitBash
+#Ok, the Canopy shell thing was wrong. Workflow as of 2014-04-08
+#Move .ps files to THIS directory; run script; move .ps & .pdfs home
 
 
-for i in $(ls *.ps);
-do ps2pdfwr $i;
+for i in $(ls *.ps); do
+    ps2pdfwr $i;
 done
 clear
